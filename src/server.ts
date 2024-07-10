4198b6cc43c9d6c86daba44bb71c35ca240f972a
vinimostaco <vini.mostaco@hotmail.com>
2024-07-10 09:22:19 -0300

    initial commit with the first structure from nlw journey class

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import fastify from "fastify";
+import { prisma } from "./lib/prisma";
+
+const app = fastify();
+
+app.get("/cadastrar", async () => {
+  await prisma.trip.create({
+    data: {
+      destination: "Florianopolis",
+      starts_at: new Date(),
+      ends_at: new Date(),
+    },
+  });
+  return "Registro cadastrado com sucesso";
+});
+
+app.get("/listar", async () => {
+  const trips = await prisma.trip.findMany();
+  return trips;
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("server running");
+});
